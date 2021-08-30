e563deea66ebcec389dd6c22dd8ea86726feeea9
Change-37 <202110305@bssm.hs.kr>
2021-08-30 20:39:09 +0900

    2021.08.30. add wadda.js for wadda_join.html

---
+++ Waddaa/wadda.js
@@ -0,0 +1,49 @@
+var user = {
+    name: "",
+    id: "",
+    mail: "",
+    password: ""
+};
+
+
+function checkInput() {
+        if(!document.joinIn.name.value) 
+        {
+            window.alert("이름을 입력해주세요.");
+            return;
+        }
+        if(!document.joinIn.mail.value) 
+        {
+            window.alert("이메일을 입력해주세요.");
+            return;
+        }
+        if(!document.joinIn.id.value) 
+        {
+            window.alert("아이디를 입력해주세요.");
+            return;
+        }
+        if(!document.joinIn.password.value) 
+        {
+            window.alert("비밀번호를 입력해주세요.");
+            return;
+        }
+        if(document.joinIn.password.value.length < 8)
+        {
+            window.alert("비밀번호가 8자 미만입니다.");
+            return;
+        }
+        if(!document.joinIn.password.value != !document.joinIn.checkpw.value) 
+        {
+            window.alert("비밀번호를 확인해주세요.");
+            return;
+        }
+        user.name = document.joinIn.name.value;
+        user.id = document.joinIn.id.value;
+        user.mail = document.joinIn.mail.value;
+        user.password = document.joinIn.password.value;
+        console.log("name =", user.name);
+        console.log("id =", user.id);
+        console.log("mail =", user.mail);
+        console.log("password =", user.password);
+        window.alert("회원가입이 완료되었습니다.");
+};
