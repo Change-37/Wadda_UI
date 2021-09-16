60e6a7d0bf6f26a2ba074217763794a6838db2e5
Change-37 <202110305@bssm.hs.kr>
2021-09-16 09:44:56 +0900

    2021-09-16 09:44

--- Waddaa/wadda.js
+++ Waddaa/wadda.js
@@ -7,43 +7,67 @@ var user = {
 
 
 function checkInput() {
-        if(!document.joinIn.name.value) 
-        {
-            window.alert("이름을 입력해주세요.");
-            return;
-        }
-        if(!document.joinIn.mail.value) 
-        {
-            window.alert("이메일을 입력해주세요.");
-            return;
-        }
-        if(!document.joinIn.id.value) 
-        {
-            window.alert("아이디를 입력해주세요.");
-            return;
-        }
-        if(!document.joinIn.password.value) 
-        {
-            window.alert("비밀번호를 입력해주세요.");
-            return;
-        }
-        if(document.joinIn.password.value.length < 8)
-        {
-            window.alert("비밀번호가 8자 미만입니다.");
-            return;
-        }
-        if(!document.joinIn.password.value != !document.joinIn.checkpw.value) 
-        {
-            window.alert("비밀번호를 확인해주세요.");
-            return;
-        }
-        user.name = document.joinIn.name.value;
-        user.id = document.joinIn.id.value;
-        user.mail = document.joinIn.mail.value;
-        user.password = document.joinIn.password.value;
+    if(!document.joinIn.name.value) 
+    {
+        window.alert("이름을 입력해주세요.");
+        joinIn.name.focus();
+    }
+    else if(!document.joinIn.id.value) 
+    {
+        window.alert("아이디를 입력해주세요.");
+        JoinIn.id.focus();
+    }
+    else if(!document.joinIn.mail.value) 
+    {
+        window.alert("이메일을 입력해주세요.");
+    }
+    else if(!document.joinIn.password.value) 
+    {
+        window.alert("비밀번호를 입력해주세요.");
+    }
+    else if(document.joinIn.password.value.length < 8 || isNaN(password.value))
+    {
+        window.alert("비밀번호가 8자 미만입니다.");
+    }
+    else if(document.joinIn.password.value != document.joinIn.checkpw.value) 
+    {
+        window.alert("비밀번호를 확인해주세요.");
+    }
+    else 
+    {
+        user.name = document.querySelector('#name');
+        user.id = document.querySelector('#id');
+        user.mail = document.querySelector('#email');
+        user.password = document.querySelector('#password');
         console.log("name =", user.name);
         console.log("id =", user.id);
         console.log("mail =", user.mail);
         console.log("password =", user.password);
-        window.alert("회원가입이 완료되었습니다.");
+
+        $.ajax({
+            url: './checkLogin.php',
+            data: {
+                userName: user.name,
+                userId: user.id,
+                userMail: user.mail,
+                userPw: user.password
+            },
+            type: "POST",
+            dataType: "json",
+            success: function(data){
+                if(data.succ)
+                	alert("로그인 성공");
+                else
+                    alert("로그인 실패");
+            }, 
+            error: function(err){
+				alert(err);
+            }
+        });
+
+        alert("회원가입이 완료되었습니다.");
+    }
 };
+
+
+
